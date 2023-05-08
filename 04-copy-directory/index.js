6a8addfda0cd6db3223ad28669405b94e2539e9b
error404-2 <kapwas@gmail.com>
2023-05-08 07:45:28 +0300

    feat: solve task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+const fromFolder = path.resolve(__dirname, 'files');
+const copyDir = path.resolve(__dirname, 'files-copy');
 
+fs.access(copyDir, function (error) {
+  if (!error) {
+    fs.rm(copyDir, { recursive: true }, () => {
+      copyFolder(fromFolder, copyDir);
+    }
+    );
+  } else {
+    copyFolder(fromFolder, copyDir);
+  }
+})
+
+function copyFolder(from, target) {
+  fs.mkdir(target, { recursive: true }, (err) => { if (err) throw err });
+  fs.readdir(from, { withFileTypes: true }, (error, dirList) => {
+    if (!error) {
+      dirList.forEach((content) => {
+        if (content.isDirectory()) {
+          copyFolder(path.resolve(from, content.name), path.resolve(target, content.name));
+        }
+        if (content.isFile()) {
+          fs.copyFile(path.resolve(from, content.name), path.resolve(target, content.name), (err) => { if (err) throw err });
+        }
+      });
+    } else {
+      console.log(error);
+    }
+  });
+}
